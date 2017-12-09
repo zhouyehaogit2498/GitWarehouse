cca1f66a2503f44fbb48a1e86f2bbeec3b5c3ca0
zhouyehaogit2498 <34386818+zhouyehaogit2498@users.noreply.github.com>
2017-12-09 17:44:53 +0800

    测试乱码
    测试

--- Com/src/com/Main2.java
+++ Com/src/com/Main2.java
@@ -3,6 +3,7 @@ package com;
 
 public class Main2 {
 	public static void main(String[] args) {
+		System.out.println("哇爪Q");
 		 StringBuffer sb = new StringBuffer();
 		 sb.append("111").append("&&").append("ccc").append("&&");
 		 System.out.println(sb.toString());
