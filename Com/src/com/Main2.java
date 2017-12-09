ae28bf19bcb42f8f1b6df70ba6837453daa87e3e
zhouyehaogit2498 <2498230018@qq.com>
2017-12-09 17:49:49 +0800

    测试

--- Com/src/com/Main2.java
+++ Com/src/com/Main2.java
@@ -3,7 +3,7 @@ package com;
 
 public class Main2 {
 	public static void main(String[] args) {
-		System.out.println("哇爪Q");
+		System.out.println("哇爪Q 喷他Q");
 		 StringBuffer sb = new StringBuffer();
 		 sb.append("111").append("&&").append("ccc").append("&&");
 		 System.out.println(sb.toString());
